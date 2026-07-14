b44e0a49a653a2f1a7a18fbbd1bf0664b28aa58e
Ayush0390 <ayushdhane948@gmail.com>
2026-07-14 12:48:21 +0530

    Test birthday celebration

--- script.js
+++ script.js
@@ -186,7 +186,7 @@ const today = new Date();
 
 let year = today.getFullYear();
 
-let birthday = new Date(year,6,28);
+let birthday = new Date(new Date().getTime() + 60000);
 
 if(today > birthday){
 
