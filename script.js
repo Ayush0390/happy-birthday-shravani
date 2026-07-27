84a0a9c6074a5dc2470bac5b1ee4c98f1e48c4d1
Ayush0390 <ayushdhane948@gmail.com>
2026-07-28 00:29:48 +0530

    Fix birthday countdown for birthday day

--- script.js
+++ script.js
@@ -244,36 +244,6 @@ if (today.getTime() === birthdayToday.getTime()) {
 
 }
 
-const countdownInterval = setInterval(()=>{
-
-    const now = new Date().getTime();
-
-    const distance = birthday-now;
-
-    if(distance<=0){
-
-    clearInterval(countdownInterval);
-
-    birthdayCelebration();
-
-    return;
-
-}
-
-    const days=Math.floor(distance/(1000*60*60*24));
-
-    const hours=Math.floor((distance%(1000*60*60*24))/(1000*60*60));
-
-    const minutes=Math.floor((distance%(1000*60*60))/(1000*60));
-
-    const seconds=Math.floor((distance%(1000*60))/1000);
-
-    document.getElementById("days").innerHTML=days;
-    document.getElementById("hours").innerHTML=hours;
-    document.getElementById("minutes").innerHTML=minutes;
-    document.getElementById("seconds").innerHTML=seconds;
-
-},1000);
 
 // =====================================================
 // SCRIPT.JS - PART 2
